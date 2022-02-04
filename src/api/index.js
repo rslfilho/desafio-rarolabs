353a47a96c7bf87a2ebeeeb3abd7c4e2c5d23224
Roberval Filho <rslfilho@gmail.com>
2022-02-04 18:11:22 -0300

    feat : adicionando middleware de erro

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 
 const root = require('../routers/root');
+const { error } = require('../middlewares');
 
 const { PORT } = process.env;
 
@@ -11,5 +12,7 @@ const app = express();
 app.use(cors());
 
 app.use(root);
+
+app.use(error);
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
