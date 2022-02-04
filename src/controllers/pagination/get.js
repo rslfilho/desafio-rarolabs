1ca46f8fab5076b3f2a352b7bca07c1ab8f7f68a
Roberval Filho <rslfilho@gmail.com>
2022-02-04 18:49:17 -0300

    feat : adicionando chamada ao Service no Controller e tratando possível error

--- src/controllers/pagination/get.js
+++ src/controllers/pagination/get.js
@@ -1,11 +1,15 @@
+const paginationService = require('../../services/pagination');
 const { errors } = require('../../helpers');
 
 module.exports = async (req, res, next) => {
   try {
-    const { paginaAtual, quantidadePaginas } = req.query;
-    if (!paginaAtual || !quantidadePaginas) {
+    const { paginaAtual: page, quantidadePaginas: pageQuantity } = req.query;
+    if (!page || !pageQuantity) {
       return next(errors.missingParams);
     }
+
+    const response = await paginationService.get(page, pageQuantity);
+    if ('code' in response) return next(errors.invalidParams);
 
     res.status(200).end();
   } catch (e) {
