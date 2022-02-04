b238f10de8d46615873dbe6b0afb830ce79b31d2
Roberval Filho <rslfilho@gmail.com>
2022-02-04 19:05:41 -0300

    fix : padronizando sintaxe do if

--- src/controllers/pagination/get.js
+++ src/controllers/pagination/get.js
@@ -4,12 +4,10 @@ const { errors } = require('../../helpers');
 module.exports = async (req, res, next) => {
   try {
     const { paginaAtual: page, quantidadePaginas: pageQuantity } = req.query;
-    if (!page || !pageQuantity) {
-      return next(errors.missingParams);
-    }
+    if (!page || !pageQuantity) return next(errors.missingParams);
 
-    const response = await paginationService.get(page, pageQuantity);
-    if ('code' in response) return next(errors.invalidParams);
+    const response = await paginationService.get(+page, +pageQuantity);
+    if ('code' in response) return next(errors[response.code]);
 
     return res.status(200).json(response);
   } catch (e) {
