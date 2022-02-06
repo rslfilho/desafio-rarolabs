523eacdb4b1a1feaba9b2abbc73018d99d721f72
Roberval Filho <rslfilho@gmail.com>
2022-02-06 08:40:58 -0300

    [ PR ] - Mudança de lógica de paginação (#5)

--- src/helpers/getPagination.js
+++ src/helpers/getPagination.js
@@ -1,55 +1,36 @@
+const getFirstAndLastPages = (page, pageQuantity) => {
+  let firstPage = page - 2 > 0 ? page - 2 : 1;
+  const lastPage = firstPage + 4 <= pageQuantity ? firstPage + 4 : pageQuantity;
+  firstPage = lastPage - firstPage === 4 ? firstPage : pageQuantity - 4;
+
+  return { firstPage, lastPage };
+};
+
 const setPage = (page, index) => {
   if (page === index) return `**${index}**`;
   return index.toString();
 };
 
-const getPaginationTypeOne = (page, pageQuantity) => {
+const getNumbersArray = (page, firstPage, lastPage) => {
   const result = [];
-
-  for (let index = 1; index <= pageQuantity; index += 1) {
+  for (let index = firstPage; index <= lastPage; index += 1) {
     result.push(setPage(page, index));
   }
-
   return result;
 };
 
-const getPaginationTypeTwo = (page) => {
-  const result = [];
-  
-  for (let index = 1; index <= 5; index += 1) {
-    result.push(setPage(page, index));
-  }
-
-  result.push('...');
-
-  return result;
-};
-
-const getPaginationTypeThree = (page, pageQuantity) => {
-  const result = ['...'];
-
-  for (let index = pageQuantity - 4; index <= pageQuantity; index += 1) {
-    result.push(setPage(page, index));
-  }
-  
-  return result;
-};
-
-const getPaginationTypeFour = (page) => {
-  const result = ['...'];
-
-  for (let index = page - 2; index <= page + 2; index += 1) {
-    result.push(setPage(page, index));
-  }
-
-  result.push('...');
-
-  return result;
+const addEllipsis = (array, pageQuantity) => {
+  if (array[0] > 1) array.unshift('...');
+  if (array[array.length - 1] < pageQuantity) array.push('...');
+  return array;
 };
 
 module.exports = (page, pageQuantity) => {
-  if (pageQuantity <= 5) return getPaginationTypeOne(page, pageQuantity);
-  if (page < 4) return getPaginationTypeTwo(page);
-  if (page > pageQuantity - 3) return getPaginationTypeThree(page, pageQuantity);
-  return getPaginationTypeFour(page);
+  if (pageQuantity <= 5) return getNumbersArray(page, 1, pageQuantity);
+
+  const { firstPage, lastPage } = getFirstAndLastPages(page, pageQuantity);
+  const numbersArray = getNumbersArray(page, firstPage, lastPage);
+  const pagination = addEllipsis(numbersArray, pageQuantity);
+
+  return pagination;
 };
