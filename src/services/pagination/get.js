8038d23feda9446a999884999987ca67e5b1443f
Roberval Filho <rslfilho@gmail.com>
2022-02-06 09:15:37 -0300

    [ PR ] - Refatorando forma de lançar erros esperados (#6)
    * refac : mudando forma de gerar erros
    
    * test : refatorando testes para nova forma de lançar erros

--- src/services/pagination/get.js
+++ src/services/pagination/get.js
@@ -1,11 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
 const validate = require('../validate');
-const { getPagination } = require('../../helpers');
+const { getPagination, errors } = require('../../helpers');
 
 module.exports = async (page, pageQuantity) => {
   const { error } = validate.queryParams({ page, pageQuantity });
-  if (error) return { code: 'invalidParams' };
-  if (page > pageQuantity) return { code: 'pageBigger' };
+  if (error) throw errors.invalidParams;
+  if (page > pageQuantity) throw errors.pageBigger;
   
   const pagination = getPagination(page, pageQuantity);
   const id = uuidv4();
